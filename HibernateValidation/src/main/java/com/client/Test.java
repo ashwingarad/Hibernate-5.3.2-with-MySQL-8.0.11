138632556589938cecc9348377ee67f09f5c6722
Ashwinkumar Garad <41720561+ashwingarad@users.noreply.github.com>
2018-08-09 14:19:35 +0530

    Update Test.java

--- HibernateValidation/src/main/java/com/client/Test.java
+++ HibernateValidation/src/main/java/com/client/Test.java
@@ -1,5 +1,15 @@
 package com.client;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Set;
+
+import javax.validation.Configuration;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
 import org.hibernate.Session;
 import org.hibernate.resource.transaction.spi.TransactionStatus;
 
@@ -8,12 +18,27 @@ import com.model.Employee;
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException, NoSuchMethodException, SecurityException {
 		Session session = HibernateUtil.getSession();
 
 		Employee employee = new Employee();
 		employee.setName("Abraham Lincoln");
 		employee.setSalary(4562132);
+
+		Configuration<?> config = Validation.byDefaultProvider().configure();
+		config.addMapping(new FileInputStream("employeeXMLValidation.xml"));
+		ValidatorFactory validatorFactory1 = config.buildValidatorFactory();
+		Validator validator1 = validatorFactory1.getValidator();
+
+		Set<ConstraintViolation<Employee>> validationErrors1 = validator1.validate(employee);
+
+		if (!validationErrors1.isEmpty()) {
+			for (ConstraintViolation<Employee> error : validationErrors1) {
+				System.out.println(error.getMessageTemplate() + "::" + error.getInvalidValue() + "::"
+						+ error.getPropertyPath() + "::" + error.getMessage());
+			}
+		}
+
 		session.save(employee);
 		session.beginTransaction().commit();
 
