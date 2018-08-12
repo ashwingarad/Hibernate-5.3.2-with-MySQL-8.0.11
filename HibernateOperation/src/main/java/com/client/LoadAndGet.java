34d8d005be0a549776733782083467b14e2ad935
Ashwinkumar Garad <41720561+ashwingarad@users.noreply.github.com>
2018-08-12 16:39:41 +0530

    Update LoadAndGet.java

--- HibernateOperation/src/main/java/com/client/LoadAndGet.java
+++ HibernateOperation/src/main/java/com/client/LoadAndGet.java
@@ -10,13 +10,21 @@ public class LoadAndGet {
 	public static void main(String[] args) {
 		Session session = HibernateUtil.getSession();
 
-		Student s1 = (Student) session.get(Student.class, 1l);
-		System.out.println(s1.getId() + "\t" + s1.getName() + "\t" + s1.getAddr());
+		try {
+			Student s2 = (Student) session.load(Student.class, 1l);
+			System.out.println("Student load() called");
+			System.out.println("ID : " + s2.getId());
+			System.out.println("Student Load : " + s2 + "\n");
 
-		Student s2 = (Student) session.load(Student.class, 1l);
-		System.out.println(s2.getId() + "\t" + s2.getName() + "\t" + s2.getAddr());
+			Student s1 = (Student) session.get(Student.class, 2l);
+			System.out.println("Student get() called");
+			System.out.println("ID : " + s1.getId());
+			System.out.println("Student Load : " + s1 + "\n");
 
-		session.close();
-		HibernateUtil.closeFactory();
+			session.close();
+			HibernateUtil.closeFactory();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
